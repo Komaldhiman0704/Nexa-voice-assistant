3306ec2be250ead65f441031e2ec2d715a2c0208
KOMAL <dhimankomal443@gmail.com>
2025-06-07 15:38:10 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,8 @@
+  
+music = {
+"blinding_lights": "https://www.youtube.com/watch?v=4NRXx6U8ABQ",
+    "save_your_tears": "https://www.youtube.com/watch?v=XXYlFuWEuKI",
+    "starboy": "https://www.youtube.com/watch?v=34Na4j8AVgA",
+    "creepin": "https://www.youtube.com/watch?v=GpP0zIj4e-4",
+    "call_out_my_name": "https://www.youtube.com/watch?v=M_5cTCJ9CvI"
+}
